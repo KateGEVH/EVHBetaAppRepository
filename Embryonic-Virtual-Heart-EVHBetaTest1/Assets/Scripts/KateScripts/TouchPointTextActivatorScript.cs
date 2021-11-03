22804dd43229cee0a03e58a4b8a3e8fd20e507e7
KateGEVH <kategustilo@mac.com>
2021-11-03 13:09:26 -0600

    latest version of beta with videos and scrubbers

--- Embryonic-Virtual-Heart-EVHBetaTest1/Assets/Scripts/KateScripts/TouchPointTextActivatorScript.cs
+++ Embryonic-Virtual-Heart-EVHBetaTest1/Assets/Scripts/KateScripts/TouchPointTextActivatorScript.cs
@@ -8,6 +8,16 @@ public class TouchPointTextActivatorScript : MonoBehaviour
     public GameObject touchpoint;
     public GameObject textpopup;
     public Button closebtn;
+    public GameObject openpopup1;
+    public GameObject openpopup2;
+    public GameObject openpopup3;
+    public GameObject openpopup4;
+    public GameObject openpopup5;
+    public GameObject openpopup6;
+    public GameObject openpopup7;
+    public GameObject openpopup8;
+    public GameObject openpopup9;
+    public GameObject openpopup10;
 
 
     //Start is called before the first frame update
@@ -21,7 +31,36 @@ public class TouchPointTextActivatorScript : MonoBehaviour
 
     private void OnMouseDown()
     {
-        
+        if (openpopup1 == isActiveAndEnabled)
+            openpopup1.SetActive(false);
+
+        if (openpopup2 == isActiveAndEnabled)
+            openpopup2.SetActive(false);
+
+        if (openpopup3 == isActiveAndEnabled)
+            openpopup3.SetActive(false);
+
+        if (openpopup4 == isActiveAndEnabled)
+            openpopup4.SetActive(false);
+
+        if (openpopup5 == isActiveAndEnabled)
+            openpopup5.SetActive(false);
+
+        if (openpopup6 == isActiveAndEnabled)
+            openpopup6.SetActive(false);
+
+        if (openpopup7 == isActiveAndEnabled)
+            openpopup7.SetActive(false);
+
+        if (openpopup8 == isActiveAndEnabled)
+            openpopup8.SetActive(false);
+
+        if (openpopup9 == isActiveAndEnabled)
+            openpopup9.SetActive(false);
+
+        if (openpopup10 == isActiveAndEnabled)
+            openpopup10.SetActive(false);
+
         textpopup.SetActive(true);
         Debug.Log("The sphere has been clicked");
     }
